752676f9c17e0a2b76b44de57cedae226fab5d0d
VinitraMk <vinitramk@gmail.com>
2017-11-05 17:57:23 +0530

    Segtree update

--- segtree.cpp
+++ segtree.cpp
@@ -1,6 +1,6 @@
-int getMid(int s, int e) {  return s + (e -s)/2;  }
+ll getMid(ll s, ll e) {  return s + (e -s)/2;  }
 
-int getSumUtil(int *st, int ss, int se, int qs, int qe, int si)
+ll getSumUtil(ll *st, ll ss, ll se, ll qs, ll qe, ll si)
 {
     if (qs <= ss && qe >= se)
         return st[si];
@@ -8,12 +8,12 @@ int getSumUtil(int *st, int ss, int se, int qs, int qe, int si)
     if (se < qs || ss > qe)
         return 0;
  
-    int mid = getMid(ss, se);
+    ll mid = getMid(ss, se);
     return getSumUtil(st, ss, mid, qs, qe, 2*si+1) +
            getSumUtil(st, mid+1, se, qs, qe, 2*si+2);
 }
  
-void updateValueUtil(int *st, int ss, int se, int i, int diff, int si)
+void updateValueUtil(ll *st, ll ss, ll se, ll i, ll diff, ll si)
 {
     if (i < ss || i > se)
         return;
@@ -21,60 +21,60 @@ void updateValueUtil(int *st, int ss, int se, int i, int diff, int si)
     st[si] = st[si] + diff;
     if (se != ss)
     {
-        int mid = getMid(ss, se);
+        ll mid = getMid(ss, se);
         updateValueUtil(st, ss, mid, i, diff, 2*si + 1);
         updateValueUtil(st, mid+1, se, i, diff, 2*si + 2);
     }
 }
  
-void updateValue(int arr[], int *st, int n, int i, int new_val)
+void updateValue(ll arr[], ll *st, ll n, ll i, ll new_val)
 {
     if (i < 0 || i > n-1)
     {
-        printf("Invalid Input");
+        prllf("Invalid Input");
         return;
     } 
-    int diff = new_val - arr[i]; 
+    ll diff = new_val - arr[i]; 
     arr[i] = new_val; 
     updateValueUtil(st, 0, n-1, i, diff, 0);
 }
  
-int getSum(int *st, int n, int qs, int qe)
+ll getSum(ll *st, ll n, ll qs, ll qe)
 {
     if (qs < 0 || qe > n-1 || qs > qe)
     {
-        printf("Invalid Input");
+        prllf("Invalid Input");
         return -1;
     } 
     return getSumUtil(st, 0, n-1, qs, qe, 0);
 }
  
-int constructSTUtil(int arr[], int ss, int se, int *st, int si)
+ll constructSTUtil(ll arr[], ll ss, ll se, ll *st, ll si)
 {
     if (ss == se)
     {
         st[si] = arr[ss];
         return arr[ss];
     }    
-    int mid = getMid(ss, se);
+    ll mid = getMid(ss, se);
     st[si] =  constructSTUtil(arr, ss, mid, st, si*2+1) +
               constructSTUtil(arr, mid+1, se, st, si*2+2);
     return st[si];
 }
  
-int *constructST(int arr[], int n)
+ll *constructST(ll arr[], ll n)
 {
-    int x = (int)(ceil(log2(n)));  
-    int max_size = 2*(int)pow(2, x) - 1;  
-    int *st = new int[max_size]; 
+    ll x = (ll)(ceil(log2(n)));  
+    ll max_size = 2*(ll)pow(2, x) - 1;  
+    ll *st = new ll[max_size]; 
     constructSTUtil(arr, 0, n-1, st, 0);
     return st;
 }
 int main()
 {
-    int arr[] = {1, 3, 5, 7, 9, 11};
-    int n = sizeof(arr)/sizeof(arr[0]); 
-    int *st = constructST(arr, n); 
+    ll arr[] = {1, 3, 5, 7, 9, 11};
+    ll n = sizeof(arr)/sizeof(arr[0]); 
+    ll *st = constructST(arr, n); 
     printf("Sum of values in given range = %d\n", 
             getSum(st, n, 1, 3)); 
     updateValue(arr, st, n, 1, 10); 
